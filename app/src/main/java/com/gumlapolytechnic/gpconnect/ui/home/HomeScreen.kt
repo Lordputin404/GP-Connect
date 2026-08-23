c9379d7c35147a89de3b0b0df7f68c1005024498
Lordputin404 <amarkujur4583@gmail.com>
2026-08-23 13:07:40 +0530

    fix: add missing home icon import

--- app/src/main/java/com/gumlapolytechnic/gpconnect/ui/home/HomeScreen.kt
+++ app/src/main/java/com/gumlapolytechnic/gpconnect/ui/home/HomeScreen.kt
@@ -22,6 +22,7 @@ import androidx.compose.material.icons.outlined.Groups
 import androidx.compose.material.icons.outlined.LocationOn
 import androidx.compose.material.icons.outlined.MenuBook
 import androidx.compose.material.icons.outlined.Restaurant
+import androidx.compose.material3.Icon
 import androidx.compose.material3.MaterialTheme
 import androidx.compose.material3.Surface
 import androidx.compose.material3.Text
